7ab60f96ab2644223e8ec248d324ba1b5e6207cb
John Pawlak <johnpawlak@gmail.com>
2018-10-17 15:46:05 -0400

    added yargs functionality

--- NoteApp/app.js
+++ NoteApp/app.js
@@ -2,4 +2,31 @@ console.log('starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js')
+
+//grabbing the arguments, process.argv populated command line arguments available. If you add extra command line arguments
+// on the terminal, then it will add them unto the argv parse.
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('command: ', command);
+// console.log('process.argv', process.argv)
+console.log('yargs.argv', argv)
+
+if(command === 'add') {
+    notes.addNote(argv.title, argv.body);
+}
+else if (command === 'list'){
+    notes.getAll();
+} 
+else if (command === 'read') {
+    notes.readNote(argv.title);
+}
+else if (command === 'remove') {
+    notes.remove(argv.title);
+}
+else {
+   console.log('command not recognized'); 
+} 
--- NoteApp/notes.js
+++ NoteApp/notes.js
@@ -1,6 +1,21 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+var getAll = () => {
+    console.log('Getting all notes');
+}
+var readNote = (title) => {
+    console.log('Reading note:', title);
+}
+var remove = (title) => {
+    console.log('removing note', title);
+}
+
+module.exports = {
+    addNote: addNote,
+    getAll: getAll,
+    readNote: readNote,
+    remove: remove,
 }

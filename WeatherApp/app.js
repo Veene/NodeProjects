dcda8f2939f48439fdb6f096ceb69b06ba0a6543
John Pawlak <johnpawlak@gmail.com>
2018-10-19 13:00:55 -0400

    added encodedURI and yargs functionality

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,14 +1,30 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv)
 
 const key = "key=zE73irn2qSoMthtnXR06gqYXGZCXWZdH"
-const url = `http://www.mapquestapi.com/geocoding/v1/address?${key}&location=1301%20lombard%20street%20philadelphia`
+const url = `http://www.mapquestapi.com/geocoding/v1/address?${key}&location=${encodeURIComponent(argv.address)}`
+console.log(url)
 
 request({
     url: url,
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(body, undefined, 0))
-    // console.log(JSON.stringify(body, undefined, 2))
+    console.log("Address: ", body.results[0].locations[0].street, body.results[0].locations[0].adminArea5, body.results[0].locations[0].adminArea1)
     console.log("latitude: ", body.results[0].locations[0].latLng.lat)
     console.log("longitude: ", body.results[0].locations[0].latLng.lng)
 })

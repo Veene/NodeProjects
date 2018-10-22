d0c8181a07af8ef7dd435ed85d7aefde4ae21182
John Pawlak <johnpawlak@gmail.com>
2018-10-22 07:34:09 -0400

    handlebars added, middleware added for serverlog

--- NodeWebServer/server.js
+++ NodeWebServer/server.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express(); 
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+//middleware basically below
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('unable to append to server.log')
+        }
+    })
+    next();
+})
+//this middleware with no next always renders maintanence we get nothing else
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs')
+// })
+
 app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 
 //request, response is EXPRESS specific!!!!
 app.get('/', (req, res) => {
@@ -16,8 +47,17 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'This is the home page!'
+    })
+})
+
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    })
 })
 
 app.get('/bad', (req, res) => {

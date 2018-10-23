a6ca46f31b44edf8cd86a0fb89a65829316b2373
John Pawlak <johnpawlak@gmail.com>
2018-10-23 09:36:58 -0400

    testing added with mocha and supertest

---
+++ NodeTests/utils/utils.test.js
@@ -0,0 +1,49 @@
+const utils = require('./utils');
+const expect = require('expect');
+
+it('should add two numbers', () => {
+    var res = utils.add(33,11);
+
+    expect(res).toBe(44).toBeA('number');   
+});
+
+it('should square a number', () => {
+    var res = utils.square(4);
+    expect(res).toBe(16).toBeA('number');
+})
+
+it('should expect some values', () => {
+    // expect(12).toNotBe(11)
+    // expect({name:'Andrew'}).toEqual({name:'Andrew'})
+    // expect([2,3,4]).toInclude(4)
+    // expect([2,3,4]).toExclude(1)
+    expect({
+        name: 'john',
+        age:25,
+        location: 'Toronto'
+    }).toInclude({
+        age:25
+    })
+})
+
+it('should verify first and last names are set', () => {
+    var user = {student: 'yes'}
+    var res = utils.setName({}, 'john pawlak')
+    expect({
+        firstName: 'john',
+        lastName: 'pawlak',
+    })
+})
+
+it('should async add two number', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number')
+        done();
+    })
+})
+it('should async square a number', (done) => {
+    utils.asyncSquare(3,(sum) => {
+        expect(sum).toBe(9).toBeA('number')
+        done();
+    })
+})

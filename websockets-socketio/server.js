2565ceed1603a0e03b9019dbab6cce4509c4b6cf
John Pawlak <johnpawlak@gmail.com>
2018-11-29 14:27:57 -0500

    added movement listeners and player state on server.js

--- websockets-socketio/server.js
+++ websockets-socketio/server.js
@@ -21,14 +21,45 @@ server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
 
-io.on('connection', (socket) => {
-    console.log('connected user: ')
-
-    socket.on('disconnect', () => {
-        console.log('disconnected user')
-    })
-})
-
+//THIS IS JUST TO TEST EMITS OF HI
 setInterval(() => {
     io.sockets.emit('message', 'hi!');
 }, 1000);
+
+var players = {};
+
+io.on('connection', function(socket) {
+  console.log('new user connected: ', socket.id)
+  //when client socket starts up, it emits 'new player' and we use its socket.id to place it at starting pos x:300, y:300
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  //receives movement emit 60 times a second from client
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    //data in this case is the movement object (movement.up = false||true, movement.down = false|true etc)
+    if (data.left) {
+      //players[socket.id].x -= 5;
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+  socket.on('disconnect', () => {
+      console.log('user disconnected: ', socket.id)
+  })
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
--- websockets-socketio/static/game.js
+++ websockets-socketio/static/game.js
@@ -1,5 +1,51 @@
 let socket = io();
 
+//listening for messages from the server
 socket.on('message', (data) => {
     console.log(data)
 })
+
+var movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+      case 65: // A
+        movement.left = true;
+        break;
+      case 87: // W
+        movement.up = true;
+        break;
+      case 68: // D
+        movement.right = true;
+        break;
+      case 83: // S
+        movement.down = true;
+        break;
+    }
+});
+document.addEventListener('keyup', (event) => {
+    switch (event.keyCode) {
+      case 65: // A
+        movement.left = false;
+        break;
+      case 87: // W
+        movement.up = false;
+        break;
+      case 68: // D
+        movement.right = false;
+        break;
+      case 83: // S
+        movement.down = false;
+        break;
+    }
+});
+//when game.js client loads, it emits new player, which will be listened to with server
+socket.emit('new player');
+// each client will continuously emit movement, 60/second to the server
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);

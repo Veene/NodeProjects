adafb35e82cc082e0698d6b2d620ba4094a23a8c
John Pawlak <johnpawlak@gmail.com>
2018-11-29 10:14:20 -0500

    setup express basic routing for server

--- coin-collect-socketio/server/app.js
+++ coin-collect-socketio/server/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
-	res.send(err.message || 'Internal server error');
+	res.send(err.message || 'Internal server error!');
 });
 
 io.on('connection', (socket) => {

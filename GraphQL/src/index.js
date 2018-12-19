b39f0a2bee78d10e2e6523e27c83665f92687744
John Pawlak <johnpawlak@gmail.com>
2018-12-19 00:31:54 -0500

    added schemas for scalar types

--- GraphQL/src/index.js
+++ GraphQL/src/index.js
@@ -1,10 +1,65 @@
-import myCurrentLocation, { message, name, getGreeting } from './myModule'
-import add, { subtract as minusBitch } from './math'
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreeting('John'))
-console.log(add(2,5))
-console.log(minusBitch(5,4)))
+//THE FIVE SCALAR TYPES - String, Boolean, Int, Float, ID
 
+//Type definitions (schema)
+const typeDefs = `
+    type Query {
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
+
+    }
+`
+
+//Resolvers
+const resolvers = {
+    Query: {
+        id() {
+            return `abc123`
+        },
+        name() {
+            return `John`
+        },
+        age() {
+            return 29
+        },
+        employed() {
+            return true
+        },
+        gpa() {
+            return null //we could return null because no exclamation mark!
+        },
+        title() {
+            return `Boobs`
+        },
+        price() {
+            return 3.99
+        },
+        releaseYear() {
+            return null 
+        },
+        rating() {
+            return null 
+        },
+        inStock() {
+            return true
+        },
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('server is running up and running')
+})

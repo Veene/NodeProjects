d50dd594366c1f283ea188ae3b9135062eff283c
John Pawlak <johnpawlak@gmail.com>
2018-12-24 09:46:52 -0500

    added posts author and comments to each typeDef and resolvers

--- GraphQL/src/index.js
+++ GraphQL/src/index.js
@@ -43,19 +43,23 @@ const posts = [{
 const comments = [{
     id:'1',
     text:'thats great1',
-    author: '1'
+    author: '1',
+    post: '1'
 },{
     id:'2',
     text:'thats great2',
-    author: '1'
+    author: '1',
+    post: '1'
 },{
     id:'3',
     text:'thats great3',
-    author: '2'
+    author: '2',
+    post: '2'
 },{
     id:'4',
     text:'thats great4',
-    author: '3'
+    author: '3',
+    post: '3'
 },]
 
 //Type definitions (schema)
@@ -74,6 +78,7 @@ const typeDefs = `
         email: String!
         age: Int
         posts: [Post!]!
+        comments: [Comment!]!
     }
 
     type Post {
@@ -82,10 +87,13 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
     type Comment {
         id: ID!
         text: String!
+        author: User!
+        post: Post!
     }
 `
 
@@ -131,11 +139,25 @@ const resolvers = {
     Post: {
         author(parent, args, ctx, info) {
             return users.find((user) => user.id === parent.author)
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => comment.post === parent.id)
         }
     },
     User: {
         posts(parent, args, ctx, info) {
             return posts.filter((post) => post.author === parent.id)
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => comment.author === parent.id)
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => user.id === parent.author)
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => post.id === parent.post)
         }
     }
 }

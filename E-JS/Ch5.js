f26312444b3e3078dcc0aaf28472006c31b371f6
John Pawlak <johnpawlak@gmail.com>
2019-02-02 18:34:22 -0500

    ch5

--- E-JS/Ch5.js
+++ E-JS/Ch5.js
@@ -32,3 +32,49 @@ function unless(thisTest, thenThat){
 //   unless(n % 2 === 1, () => console.log(n, " is even"))
 // })
 
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+function flatten(arr) {
+  const flattened = arr.reduce((a,b) => {
+    return a.concat(b);
+  }, [])
+  return flattened;
+}
+// console.log(flatten(arrays))
+
+// → [1, 2, 3, 4, 5, 6]
+
+function loop(start, test, update, body) {
+  for(let i=start; test(i); i = update(i)) {
+    body(i);
+  }
+}
+
+// loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
+
+function every(array, test) {
+  for(let i=0; i<array.length; i++) {
+    if(!test(array[i])) {
+      return false
+    }
+  }
+  return true;
+}
+
+function some(array, test) {
+  for(let i=0; i<array.length; i++) {
+    if(test(i)) return true
+  }
+  return false
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+

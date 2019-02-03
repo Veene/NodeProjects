bf21e98447bf1c621f350db527f73d9e9c5eeb70
John Pawlak <johnpawlak@gmail.com>
2019-02-03 13:50:37 -0500

    set up live watch and reloading with gulp

--- Gulp-website/gulpfile.js
+++ Gulp-website/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+
+//File paths
+const SCRIPTS_PATH = 'public/scripts/**/*.js';
 
 //styles
 gulp.task('styles', function() {
@@ -10,17 +14,25 @@ gulp.task('styles', function() {
 gulp.task('scripts', function(cb) {
   console.log('starting scripts task');
   //piping just means passing a file through multiple steps. this step calls uglify and then that files write/save into public/dist folder
-  gulp.src('public/scripts/*.js').pipe(uglify()).pipe(gulp.dest('public/dist'));
-  cb()
+  gulp.src(SCRIPTS_PATH).pipe(uglify()).pipe(gulp.dest('public/dist')).pipe(livereload);
+  cb();
 });
 
 //Images
 gulp.task('images', function(cb) {
   console.log('starting images task');
-  cb()
+  cb();
 });
 
 gulp.task('default', function(cb) {
   console.log('starting default task');
-  cb()
+  cb();
+});
+
+gulp.task('watch', function(cb) {
+  console.log('starting watch task');
+  require('./server.js');
+  livereload.listen();
+  gulp.watch(SCRIPTS_PATH, gulp.series('scripts'));
+  cb();
 });
---
+++ Gulp-website/server.js
@@ -0,0 +1,8 @@
+var StaticServer = require('static-server');
+
+var server = new StaticServer({
+  rootPath: './public/',
+  port: 3000
+});
+
+server.start(() => console.log('server started on port ', server.port));
